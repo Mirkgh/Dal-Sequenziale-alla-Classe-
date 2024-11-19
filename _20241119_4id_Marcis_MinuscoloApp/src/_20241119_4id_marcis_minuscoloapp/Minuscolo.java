952305f94c0adea78e6f6eaf9f91f03cbeefc773
Mirkgh <mirko762006@gmail.com>
2024-11-19 11:23:43 +0100

    Update Minuscolo.java
    aggiunti commenti

--- _20241119_4id_Marcis_MinuscoloApp/src/_20241119_4id_marcis_minuscoloapp/Minuscolo.java
+++ _20241119_4id_Marcis_MinuscoloApp/src/_20241119_4id_marcis_minuscoloapp/Minuscolo.java
@@ -13,16 +13,20 @@ import java.util.Scanner;
  * @author ospite
  */
 public class Minuscolo {
+    /*creazioni delle variabili: tanti che determina il valore massimo dell'array, parola che ci sarà riempita con quella inserita dall'utente,
+    minuscolo che è l'array dove verrà inserita la parola dell'utente
+   */
     private int TANTI = 30;
     private String parola;
     private char[] minuscolo = new char[TANTI];
-    
+    //metodo input parola che contiente il metodo scanner che ci permette di leggere la parola inserita dall'utente
     public void inputparola() {
         Scanner in = new Scanner(System.in);
         
         System.out.print("\nInserisci una parola : ");
         parola = in.next(); //legge una stringa 
     }
+        //metodo stringtoarray che trasforma la stringa nell'array
         public void stringtoarray(){
        //trasforma la stringa letta in un array di caratteri
        for (int x = 0; x < parola.length(); x++) {
@@ -30,6 +34,7 @@ public class Minuscolo {
            
        }
     }
+    //metodo parolainminuscolo che trasformerà ogni lettera contenuta nell'array in minuscolo     
     public void parolainminuscolo() {
          //trasforma la parola in minuscolo
        for (int x = 0; x<parola.length(); x++) {
@@ -43,6 +48,7 @@ public class Minuscolo {
            System.out.print(minuscolo[x]);
        }
     }
+    //metodo arraytostring che trasformerà l'array usato precedentemente in una stringa 
     public void arraytostring(){
        String parolaMinu = new String(minuscolo);
        System.out.print("\nLa parola in minuscolo (oggetto) :");
